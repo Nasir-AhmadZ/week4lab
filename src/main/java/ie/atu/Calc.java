addb03c2d84e612e49df697aa3587356f01b4637
Nasir-AhmadZ <G00419630@atu.ie>
2023-10-10 13:52:07 +0100

    added new method called add

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,24 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[]arg){
+        add();
 
     }
+
+    public static void add() {
+        System.out.println("please your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please your second number: ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("The total is " + total);
+    }
+
 }
+
